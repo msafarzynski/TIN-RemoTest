9c7f336e40d11c033adb1d246b0aee1eeb08efdf
Tomaszpiotro <tomaszpiotro@gmail.com>
2015-04-23 17:06:08 +0200

    Implemented simple Controller.

---
+++ src/controller/controller.cpp
@@ -0,0 +1,40 @@
+
+#include <typeindex>
+#include <iostream>
+#include <utility>
+
+#include "../../include/controller/event_queue.hpp"
+#include "../../include/controller/controller.h"
+
+
+Controller::Controller(EventQueue* const event_queue) : event_queue(event_queue)
+{
+    this->strategyMap.insert(std::make_pair<std::type_index, StringStrategy*>
+            (std::type_index(typeid(StringEvent)), new StringStrategy()));
+}
+
+void Controller::handle_events()
+{
+    while(true)
+    {
+        Event* event = event_queue->pop();
+        ControllerStrategy* strategy =
+            strategyMap[std::type_index(typeid(*event))];
+        strategy->react(event);
+    }
+}
+
+void StringStrategy::react(Event* event)
+{
+    StringEvent* stringEven = dynamic_cast<StringEvent*>(event);
+}
+
+ControllerStrategy::ControllerStrategy()
+{
+
+}
+
+void ControllerStrategy::react(Event* event)
+{
+
+}
