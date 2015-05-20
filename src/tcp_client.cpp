9c20d684a068fa506122bca92918f9e60206b530
agatadebska <agata.martyna.debska@gmail.com>
2015-05-20 22:42:27 +0200

    Update tcp_client.cpp

--- src/tcp_client.cpp
+++ src/tcp_client.cpp
@@ -1,54 +1,47 @@
-  #include "../include/tcp_client.h"
+#include "../include/tcp_client.h"
 
- tcp_client::tcp_client(char* char_addr, int port){
-    sockfd = socket(AF_INET6, SOCK_STREAM, 0);
-    if (sockfd == -1)
-    {
-      perror("Cannot create socket");
-      return;
-    }
-    if(strchr(char_addr, '.') != NULL){
-      hp = gethostbyname(char_addr);
-      if(hp == NULL){
-        perror("Invalid address");
-        return;
-      }
-      memset((char*)&addr4, 0, sizeof(addr4));
-      addr4.sin_family = AF_INET;
-      memcpy((char*)&addr4.sin_addr, (char*)hp->h_addr, hp->h_length);
-      addr4.sin_port = htons(port);
-      if(connect(sockfd, (struct sockaddr*)&addr4, sizeof(addr4))==-1){
-        perror("connecting stream socket");
-        close(sockfd);
-        return;
-      }
-    }
-    else if(strchr(char_addr, ':') != NULL){
-      hp = gethostbyname2(char_addr, AF_INET6);
-      if(hp == NULL){
-        perror("Invalid address");
-        return;
-      }
-      memset((char*)&addr6, 0, sizeof(addr6));
-      //addr6.sin6_len = sizeof(addr6);
-      memcpy((char*)&addr6.sin6_addr, (char*)hp->h_addr, hp->h_length);
-      addr6.sin6_family = AF_INET6;
-      addr6.sin6_port = htons(port);
-      if(connect(sockfd, (struct sockaddr*)&addr6, sizeof(addr6))==-1){
-        perror("connecting stream socket");
-        close(sockfd);
-        return;
-      }
-    }
- }
- tcp_client::~tcp_client(){
-  close(sockfd);
- }
- 
- 
- int tcp_client::write_msg(const char* msg){
-    if(write(sockfd, msg, strlen(msg) == -1){
-      perror("Writing on stream socket");
-      return 1;
-    }
-  }
+tcp_client::tcp_client(struct sockaddr_storage* addr){
+	struct addrinfo hints;
+	struct addrinfo *res;
+	char* server, *hisservice;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	if(getaddrinfo(server, hisservice, &hints, &res) != 0){
+		perror("error getaddrinfo");
+		return;
+	}
+
+	sockfd = socket(res->ai_family, SOCK_STREAM, 0);
+	if (sockfd == -1){
+		perror("Cannot create socket");
+		return;
+	}
+
+	if(connect(sockfd, (struct sockaddr*)&server, sizeof(struct sockaddr))==-1){
+			perror("connecting stream socket");
+			close(sockfd);
+			return;
+		}
+
+}
+
+tcp_client::~tcp_client(){
+	close(sockfd);
+}
+
+int tcp_client::send_msg(const char* msg){
+	if(send(sockfd, msg, strlen(msg), 0) == -1){
+		perror("Writing on stream socket");
+		return 1;
+		}
+	}
+
+int tcp_client::receive_msg(char* msg){
+	if(recv(sockfd, msg, 1024, 0) == -1){
+		perror("error reading from socket");
+		return 1;
+	}
+}
+
