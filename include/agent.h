8c676e7ff424c4f8ce6b6d114ea7c3708837ee1b
agatadebska <agata.martyna.debska@gmail.com>
2015-05-23 22:57:18 +0200

    Update agent.h

--- include/agent.h
+++ include/agent.h
@@ -1,11 +1,23 @@
-#pragma once
+#ifndef TIN_AGENT_H
+#define TIN_AGENT_H
+
+#include "remoTestMessage.h"
 #include "tcp_server.h"
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <signal.h>
 
 class agent{
 	tcp_server tcp_module;
+	pid_t command_pid;
 public:
 	agent();
 	~agent();
 	int send_msg(const char* msg);
 	int receive_msg(char* msg);
+	int execute_command(const char* command);
+	int kill_process();
 };
+
+#endif
