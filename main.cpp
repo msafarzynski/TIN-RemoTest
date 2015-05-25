40c87c029dabd6bb9dfcaabc94b1538c01349977
Tomaszpiotro <tomaszpiotro@gmail.com>
2015-05-26 00:05:20 +0200

    Added example Controller usage.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include "include/controller/event_queue.hpp"
+#include "include/controller/controller.h"
 
 using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
+    EventQueue* queue = new EventQueue();
+    Controller* controller = new Controller(queue);
+    controller->run();
     return 0;
 }
