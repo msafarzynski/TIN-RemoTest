ff50d42dddc1d259663f2ed8d589aac9758e3fea
agatadebska <agata.martyna.debska@gmail.com>
2015-05-25 13:18:54 +0200

    Update remoTestMessage.cpp

--- src/remoTestMessage.cpp
+++ src/remoTestMessage.cpp
@@ -14,3 +14,15 @@ remoTestMessage::remoTestMessage(char* message){
 std::string remoTestMessage::getCommand(){
     return toolName + " " + options;        //moze srednik???
 }
+
+std::string remoTestMessage::getResponse(){
+    return "1" + std::to_string(getToolId()) + options;
+}
+
+int remoTestMessage::getToolId() {
+    for(int i=0; i < tool.stringTool->size();i++){
+        if(tool.stringTool[i] == toolName)
+            return i;
+    }
+    return -1;
+}
