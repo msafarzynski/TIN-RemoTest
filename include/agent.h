3a8ff1f9d4c2ac2dbe9f6ab0f78c35fe47bf16f9
agatadebska <agata.martyna.debska@gmail.com>
2015-05-25 13:21:55 +0200

    Update agent.h

--- include/agent.h
+++ include/agent.h
@@ -7,17 +7,26 @@
 #include <cstdio>
 #include <cstdlib>
 #include <signal.h>
+#include <pthread.h>
 
 class agent{
 	tcp_server tcp_module;
 	pid_t command_pid;
+	pthread_t thread;
 public:
 	agent();
 	~agent();
 	int send_msg(const char* msg);
 	int receive_msg(char* msg);
-	int execute_command(const char* command);
 	int kill_process();
 };
+
+struct Targs{				//struktura przechowujaca argumenty dla funkcji watku
+	FILE* pipe;
+	agent* agent1;
+	remoTestMessage message;
+};
+
+void* get_output(void*);		//dla podanego Targs zwraca wynik skryptu
 
 #endif
