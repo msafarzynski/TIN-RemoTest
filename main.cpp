e80231ec7b95b753fb5b9d0e2305032ab8d562d6
Tomaszpiotro <tomaszpiotro@gmail.com>
2015-05-27 13:02:41 +0200

    Added example timer usage.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "include/controller/event_queue.hpp"
 #include "include/controller/controller.h"
+#include "include/controller/timer.h"
 
 using namespace std;
 
@@ -8,6 +9,8 @@ int main() {
     cout << "Hello, World!" << endl;
     EventQueue* queue = new EventQueue();
     Controller* controller = new Controller(queue);
+    Timer* timer = new Timer(dynamic_cast<Event*>(new StringEvent(string("mess"))), queue, 2000);
+    timer->start();
     controller->run();
     return 0;
 }
