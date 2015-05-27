de7e21339d824cacbed704dd57d6fcb4435bfe33
agatadebska <agata.martyna.debska@gmail.com>
2015-05-27 10:56:03 +0200

    Dzialajace polaczenie

--- src/tcp_server.cpp
+++ src/tcp_server.cpp
@@ -9,28 +9,46 @@ tcp_server::tcp_server(){
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_flags = AI_PASSIVE;
 	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = 0;
+	hints.ai_canonname = NULL;
+	hints.ai_addr = NULL;
+	hints.ai_next = NULL;
 
-	if(getaddrinfo(NULL, myservice, &hints, &res) != 0){
-		perror("error getaddrinfo");
+	int s = getaddrinfo(NULL, "3490", &hints, &result);
+	if(s != 0){
+		perror(gai_strerror(s));
 		return;
 	}
 
-	sockfd = socket(res->ai_family, SOCK_STREAM, 0);
+	for (rp = result; rp != NULL; rp = rp->ai_next) {
 
-	if(-1 == sockfd) {
-		perror("can not create socket");
+		sockfd = socket(rp->ai_family, rp->ai_socktype,
+					 rp->ai_protocol);
+		if (sockfd == -1)
+			continue;
+
+		if (bind(sockfd, rp->ai_addr, rp->ai_addrlen) == 0) {
+			std::cout << "Binded socket" << std::endl;
+			break;                  /* Success */
+		}
+		close(sockfd);
+	}
+	if (rp == NULL) {
+		// looped off the end of the list with no successful bind
+		fprintf(stderr, "failed to bind socket\n");
 		return;
 	}
-	if(bind(sockfd, (struct sockaddr*) res->ai_addr, sizeof(struct sockaddr)) == -1) {
-		perror("error binding the socket");
-		return;
-	}
+	freeaddrinfo(result);
+
 	listen(sockfd, 5);
+
+	std::cout << "Server is ready" << std::endl;
 	msgsock = accept(sockfd, (struct sockaddr*) 0, 0);
 	if(msgsock == -1){
 		perror("Accept");
 		return;
 	}
+	std::cout << "Connection accepted" << std::endl;
 	else printf("Server is ready");
 }
 
