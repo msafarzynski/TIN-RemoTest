8f61a24b9a0d2bde3a1595e02c168cd76e578f21
Tomaszpiotro <tomaszpiotro@gmail.com>
2015-05-31 19:20:59 +0200

    Implemented AgentController.

---
+++ include/agent/agent_controller.h
@@ -0,0 +1,11 @@
+#include "../controller/controller.h"
+
+#ifndef TIN_REMOTEST_AGENT_CONTROLLER_H
+#define TIN_REMOTEST_AGENT_CONTROLLER_H
+
+class AgentController : public Controller
+{
+public:
+    AgentController(EventQueue* const);
+};
+#endif //TIN_REMOTEST_AGENT_CONTROLLLER_H
---
+++ src/agent/agent_controller.cpp
@@ -0,0 +1,6 @@
+#include "../../include/agent/agent_controller.h"
+#include "../../include/controller/event_queue.hpp"
+
+AgentController::AgentController(EventQueue * const event_queue): Controller(event_queue)
+{
+}
