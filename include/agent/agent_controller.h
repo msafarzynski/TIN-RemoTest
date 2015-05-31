0e19dd05771cf701dfdc93752aac0a3d43e5417a
Tomaszpiotro <tomaszpiotro@gmail.com>
2015-05-31 23:15:50 +0200

    Added ExecutionStrategies.

--- include/agent/agent_controller.h
+++ include/agent/agent_controller.h
@@ -8,4 +8,19 @@ class AgentController : public Controller
 public:
     AgentController(EventQueue* const);
 };
+
+
+class StartExecutionStrategy: public ControllerStrategy
+{
+public:
+    virtual void react(Event * event);
+};
+
+
+class StartExecutionAtTimeStrategy: public ControllerStrategy
+{
+public:
+    virtual void react(Event * event);
+};
+
 #endif //TIN_REMOTEST_AGENT_CONTROLLLER_H
--- src/agent/agent_controller.cpp
+++ src/agent/agent_controller.cpp
@@ -1,6 +1,25 @@
+#include <iostream>
 #include "../../include/agent/agent_controller.h"
 #include "../../include/controller/event_queue.hpp"
 
 AgentController::AgentController(EventQueue * const event_queue): Controller(event_queue)
 {
+    this->strategyMap.insert(std::make_pair<std::type_index, StartExecutionStrategy*>
+                                     (std::type_index(typeid(StartExecutionEvent)), new StartExecutionStrategy()));
+    this->strategyMap.insert(std::make_pair<std::type_index, StartExecutionAtTimeStrategy*>
+                                     (std::type_index(typeid(StartExecutionEventAtTime)),
+                                      new StartExecutionAtTimeStrategy()));
+
+}
+
+
+void StartExecutionStrategy::react(Event *event)
+{
+    std::cout<<"Start execution strategy"<<std::endl;
+}
+
+void StartExecutionAtTimeStrategy::react(Event *event)
+{
+    StartExecutionEventAtTime* execution_event = dynamic_cast<StartExecutionEventAtTime*>(event);
+    std::cout<<"Start at time execution strategy. Time: "<<execution_event->get_time()<<std::endl;
 }
