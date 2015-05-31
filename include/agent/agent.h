82d9a9459c20d33ae8940e0e3bd04ea7f19db007
Tomaszpiotro <tomaszpiotro@gmail.com>
2015-05-31 19:24:54 +0200

    Removed unnecessary stuff from Agent.

--- include/agent/agent.h
+++ include/agent/agent.h
@@ -1,24 +1,8 @@
 #ifndef TIN_AGENT_H
 #define TIN_AGENT_H
 
-#include "remoTestMessage.h"
-#include "tcp_server.h"
-#include <string>
-#include <cstdio>
-#include <cstdlib>
-#include <signal.h>
-#include <pthread.h>
-
 class Agent {
-	tcp_server tcp_module;
-	pid_t command_pid;
-	pthread_t thread;
 public:
 	Agent();
-	~Agent();
-	int send_msg(const char* msg);
-	int receive_msg(char* msg);
-	int kill_process();
-	void execute();
 };
 #endif
