3fa15623f7f41ad78360a708e66432a489b2e6f2
Tomaszpiotro <tomaszpiotro@gmail.com>
2015-06-07 18:33:31 +0200

    Added example Delayed execution example.

--- src/agent/agent.cpp
+++ src/agent/agent.cpp
@@ -2,7 +2,6 @@
 #include "agent/agent.h"
 #include "controller/event_queue.hpp"
 #include "agent/agent_controller.h"
-#include "controller/timer.hpp"
 
 Agent::Agent()
 {
@@ -15,11 +14,13 @@ int main() {
     AgentController* controller = new AgentController(queue, model);
 
     string script = "ping 8.8.8.8";
+    std::time_t result = std::time(0) + 2000;
+
     queue->push(new UpdateScriptEvent(script));
-    queue->push(new StartExecutionEvent());
-    AgentTimer* timer = new AgentTimer(dynamic_cast<AgentEvent*>(new StopExecutionEvent()), queue, 2000);
+    queue->push(new StartExecutionAtTimeEvent(result));
+    AgentTimer* timer = new AgentTimer(dynamic_cast<AgentEvent*>(new StopExecutionEvent()), queue, 4000);
     timer->start();
-    AgentTimer* timer2 = new AgentTimer(dynamic_cast<AgentEvent*>(new PrintExecutionResultsEvent()), queue, 5000);
+    AgentTimer* timer2 = new AgentTimer(dynamic_cast<AgentEvent*>(new PrintExecutionResultsEvent()), queue, 6000);
     timer2->start();
 
     controller->run();
